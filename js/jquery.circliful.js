94b34caa17f0603a489d6ab23a37cae13e73f896
DarushDev <darushfaqiri@gmail.com>
2017-08-03 02:25:02 +0800

    Update circliful library

--- js/jquery.circliful.js
+++ js/jquery.circliful.js
@@ -1,11 +1,10 @@
-"use strict";
-
 (function ($) {
 
     $.fn.circliful = function (options, callback) {
 
         var settings = $.extend({
             // These are the defaults.
+            //startDegree: 0,
             foregroundColor: "#3498DB",
             backgroundColor: "#ccc",
             pointColor: "none",
@@ -25,8 +24,6 @@
             start: 0,
             showPercent: 1,
             percentageTextSize: 22,
-            percentageX: 100,
-            percentageY: 113,
             textAdditionalCss: '',
             targetPercent: 0,
             targetTextSize: 17,
@@ -34,56 +31,44 @@
             text: null,
             textStyle: null,
             textColor: '#666',
-            textY: null,
-            textX: null,
             multiPercentage: 0,
             percentages: null,
             textBelow: false,
             noPercentageSign: false,
             replacePercentageByText: null,
-            halfCircle: false,
-            animateInView: false,
-            decimals: 0,
-            alwaysDecimals: false,
-            title: 'Circle Chart',
-            description: '',
-            progressColor: null
+            halfCircle: false
         }, options);
 
         return this.each(function () {
             var circleContainer = $(this);
-
-            mergeDataAttributes(settings, circleContainer.data());
-
             var percent = settings.percent;
             var iconY = 83;
             var iconX = 100;
-            var percentageY = settings.percentageY;
-            var percentageX = settings.percentageX;
+            var textY = 110;
+            var textX = 100;
             var additionalCss;
             var elements;
             var icon;
             var backgroundBorderWidth = settings.backgroundBorderWidth;
-            var progressColor = settings.progressColor
 
             if (settings.halfCircle) {
                 if (settings.iconPosition == 'left') {
                     iconX = 80;
                     iconY = 100;
-                    percentageX = 117;
-                    percentageY = 100;
+                    textX = 117;
+                    textY = 100;
                 } else if (settings.halfCircle) {
                     iconY = 80;
-                    percentageY = 100;
+                    textY = 100;
                 }
             } else {
                 if (settings.iconPosition == 'bottom') {
                     iconY = 124;
-                    percentageY = 95;
+                    textY = 95;
                 } else if (settings.iconPosition == 'left') {
                     iconX = 80;
                     iconY = 110;
-                    percentageX = 117;
+                    textX = 117;
                 } else if (settings.iconPosition == 'middle') {
                     if (settings.multiPercentage == 1) {
                         if (typeof settings.percentages == "object") {
@@ -92,53 +77,51 @@
                             iconY = 110;
                             elements = '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="133" y1="50" x2="140" y2="40" stroke-width="2"  /></g>';
                             elements += '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="140" y1="40" x2="200" y2="40" stroke-width="2"  /></g>';
-                            percentageX = 228;
-                            percentageY = 47;
+                            textX = 228;
+                            textY = 47;
                         }
                     } else {
                         iconY = 110;
                         elements = '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="133" y1="50" x2="140" y2="40" stroke-width="2"  /></g>';
                         elements += '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="140" y1="40" x2="200" y2="40" stroke-width="2"  /></g>';
-                        percentageX = 170; // To center the percentage exactly in the center.
-                        percentageY = 35;
+                        textX = 175;
+                        textY = 35;
                     }
                 } else if (settings.iconPosition == 'right') {
                     iconX = 120;
                     iconY = 110;
-                    percentageX = 80;
-                } else if (settings.iconPosition == 'top' && settings.icon != 'none') {
-                    percentageY = 120;
+                    textX = 80;
                 }
             }
 
-            if (settings.targetPercent > 0 && settings.halfCircle != true) {
-                percentageY = 95;
+            if (settings.targetPercent > 0) {
+                textY = 95;
                 elements = '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="75" y1="101" x2="125" y2="101" stroke-width="1"  /></g>';
-                elements += '<text text-anchor="middle" x="' + percentageX + '" y="120" style="font-size: ' + settings.targetTextSize + 'px;" fill="' + settings.targetColor + '">' + settings.targetPercent + (settings.noPercentageSign && settings.replacePercentageByText == null ? '' : '%') + '</text>';
+                elements += '<text text-anchor="middle" x="' + textX + '" y="120" style="font-size: ' + settings.targetTextSize + 'px;" fill="' + settings.targetColor + '">' + settings.targetPercent + (settings.noPercentageSign && settings.replacePercentageByText == null ? '' : '%') + '</text>';
                 elements += '<circle cx="100" cy="100" r="69" fill="none" stroke="' + settings.backgroundColor + '" stroke-width="3" stroke-dasharray="450" transform="rotate(-90,100,100)" />';
-                elements += '<circle cx="100" cy="100" r="69" fill="none" stroke="' + settings.targetColor + '" stroke-width="3" stroke-dasharray="' + (435 / 100 * settings.targetPercent) + ', 20000" transform="rotate(-90,100,100)" />';
+                elements += '<circle cx="100" cy="100" r="69" fill="none" stroke="' + settings.targetColor + '" stroke-width="3" stroke-dasharray="' + (360 / 100 * settings.targetPercent) + ', 20000" transform="rotate(-90,100,100)" />';
             }
 
             if (settings.text != null) {
                 if (settings.halfCircle) {
                     if (settings.textBelow) {
-                        elements += '<text text-anchor="middle" x="' + (settings.textX != null ? settings.textX : '100') + '" y="' + (settings.textY != null ? settings.textY : '64%') + '" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
+                        elements += '<text text-anchor="middle" x="100" y="120" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
                     }
                     else if (settings.multiPercentage == 0) {
-                        elements += '<text text-anchor="middle" x="' + (settings.textX != null ? settings.textX : '100' ) + '" y="' + (settings.textY != null ? settings.textY : '115') + '" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
+                        elements += '<text text-anchor="middle" x="100" y="115" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
                     }
                     else if (settings.multiPercentage == 1) {
-                        elements += '<text text-anchor="middle" x="' + (settings.textX != null ? settings.textX : '228' ) + '" y="' + (settings.textY != null ? settings.textY : '65') + '" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
+                        elements += '<text text-anchor="middle" x="228" y="65" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
                     }
                 } else {
                     if (settings.textBelow) {
-                        elements += '<text text-anchor="middle" x="' + (settings.textX != null ? settings.textX : '100' ) + '" y="' + (settings.textY != null ? settings.textY : '99%') + '" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
+                        elements += '<text text-anchor="middle" x="100" y="190" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
                     }
                     else if (settings.multiPercentage == 0) {
-                        elements += '<text text-anchor="middle" x="' + (settings.textX != null ? settings.textX : '100' ) + '" y="' + (settings.textY != null ? settings.textY : '115') + '" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
+                        elements += '<text text-anchor="middle" x="100" y="115" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
                     }
                     else if (settings.multiPercentage == 1) {
-                        elements += '<text text-anchor="middle" x="' + (settings.textX != null ? settings.textX : '228' ) + '" y="' + (settings.textY != null ? settings.textY : '65') + '" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
+                        elements += '<text text-anchor="middle" x="228" y="65" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
                     }
                 }
             }
@@ -159,7 +142,7 @@
                             '<circle class="circle" cx="100" cy="100" r="57" class="border" fill="none" stroke="' + settings.foregroundColor + '" stroke-width="' + settings.foregroundBorderWidth + '" stroke-dasharray="0,20000" ' + rotate + ' />' +
                             '<circle cx="100" cy="100" r="' + settings.pointSize + '" fill="' + settings.pointColor + '" clip-path="url(#cut-off-bottom)" transform="rotate(-90,100,100)" />' +
                             icon +
-                            '<text class="timer" text-anchor="middle" x="' + percentageX + '" y="' + percentageY + '" style="font-size: ' + settings.percentageTextSize + 'px; ' + additionalCss + ';' + settings.textAdditionalCss + '" fill="' + settings.fontColor + '"><tspan class="number">' + (settings.replacePercentageByText == null ? 0 : settings.replacePercentageByText) + '</tspan><tspan class="percent">' + (settings.noPercentageSign || settings.replacePercentageByText != null ? '' : '%') + '</tspan></text>')
+                            '<text class="timer" text-anchor="middle" x="' + textX + '" y="' + textY + '" style="font-size: ' + settings.percentageTextSize + 'px; ' + additionalCss + ';' + settings.textAdditionalCss + '" fill="' + settings.fontColor + '">' + (settings.replacePercentageByText == null ? 0 : settings.replacePercentageByText) + (settings.noPercentageSign || settings.replacePercentageByText != null ? '' : '%') + '</text>')
                     );
             } else {
                 circleContainer
@@ -171,7 +154,7 @@
                             '<circle class="circle" cx="100" cy="100" r="57" class="border" fill="none" stroke="' + settings.foregroundColor + '" stroke-width="' + settings.foregroundBorderWidth + '" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />' +
                             '<circle cx="100" cy="100" r="' + settings.pointSize + '" fill="' + settings.pointColor + '" />' +
                             icon +
-                            '<text class="timer" text-anchor="middle" x="' + percentageX + '" y="' + percentageY + '" style="font-size: ' + settings.percentageTextSize + 'px; ' + additionalCss + ';' + settings.textAdditionalCss + '" fill="' + settings.fontColor + '"><tspan class="number">' + (settings.replacePercentageByText == null ? 0 : settings.replacePercentageByText) + '</tspan><tspan class="percent">' + (settings.noPercentageSign || settings.replacePercentageByText != null ? '' : '%') + '</tspan></text>')
+                            '<text class="timer" text-anchor="middle" x="' + textX + '" y="' + textY + '" style="font-size: ' + settings.percentageTextSize + 'px; ' + additionalCss + ';' + settings.textAdditionalCss + '" fill="' + settings.fontColor + '">' + (settings.replacePercentageByText == null ? 0 : settings.replacePercentageByText) + (settings.noPercentageSign || settings.replacePercentageByText != null ? '' : '%') + '</text>')
                     );
             }
 
@@ -200,32 +183,6 @@
             }
 
             if (settings.animation == 1) {
-                if (settings.animateInView) {
-                    $(window).scroll(function () {
-                        checkAnimation();
-                    });
-                } else {
-                    animate();
-                }
-            } else {
-                circle
-                    .attr("stroke-dasharray", calculateFill + ", 20000");
-
-                if (settings.showPercent == 1) {
-                    myTimer
-                        .find('.number')
-                        .text(text);
-                } else {
-                    myTimer
-                        .find('.number')
-                        .text(settings.target);
-                    myTimer
-                        .find('.percent')
-                        .text('');
-                }
-            }
-
-            function animate() {
                 var timer = window.setInterval(function () {
                     if ((angle) >= calculateFill) {
                         window.clearInterval(timer);
@@ -248,13 +205,9 @@
 
                     if (settings.replacePercentageByText == null) {
                         if (settings.halfCircle) {
-                            text = parseFloat((100 * angle / 360) * 2);
+                            text = parseInt((100 * angle / 360) * 2);
                         } else {
-                            text = parseFloat((100 * angle / 360));
-                        }
-                        text = text.toFixed(settings.decimals);
-                        if (!settings.alwaysDecimals && (percent == 0 || (percent > 1 && last != 1))) {
-                            text = parseInt(text);
+                            text = parseInt((100 * angle / 360));
                         }
                     }
 
@@ -263,60 +216,24 @@
 
                     if (settings.showPercent == 1) {
                         myTimer
-                            .find('.number')
-                            .text(text);
+                            .text(text + (settings.noPercentageSign || settings.replacePercentageByText != null ? '' : '%'));
                     } else {
                         myTimer
-                            .find('.number')
                             .text(summary);
-                        myTimer
-                            .find('.percent')
-                            .text('');
                     }
 
-                    if (progressColor != null) {
-                        $.each(progressColor, function (key, value) {
-                            if (angle >= key * 3.6) {
-                                circle.css({
-                                    stroke: value,
-                                    transition: 'stroke 0.1s linear'
-                                });
-                            }
-                        });
-                    }
                 }.bind(circle), interval);
-            }
+            } else {
+                circle
+                    .attr("stroke-dasharray", calculateFill + ", 20000");
 
-            function isElementInViewport() {
-                // Get the scroll position of the page.
-                var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-                var viewportTop = $(scrollElem).scrollTop();
-                var viewportBottom = viewportTop + $(window).height();
-
-                // Get the position of the element on the page.
-                var elemTop = Math.round(circle.offset().top);
-                var elemBottom = elemTop + circle.height();
-
-                return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-            }
-
-            function checkAnimation() {
-                // If the animation has already been started
-                if (circle.hasClass('start')) return;
-
-                if (isElementInViewport(circle)) {
-                    // Start the animation
-                    circle.addClass('start');
-                    setTimeout(animate, 250)
+                if (settings.showPercent == 1) {
+                    myTimer
+                        .text(text + (settings.noPercentageSign ? '' : '%'));
+                } else {
+                    myTimer
+                        .text(settings.target);
                 }
-            }
-
-            function mergeDataAttributes(settings, dataAttributes) {
-                $.each(settings, function (key, value) {
-                    if (key.toLowerCase() in dataAttributes) {
-                        settings[key] = dataAttributes[key.toLowerCase()];
-                    }
-                });
             }
         });
     }
